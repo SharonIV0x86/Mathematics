0a515d24b4f03de54e8905a24aa5a4057f540e9f
SharonIV <91114837+SharonIV0x86@users.noreply.github.com>
2021-09-24 12:21:23 +0530

    Update AreaOfObjects.cpp
    Before- The objects calculations and functions were taking place in only one single class, like the calculations of Square,Rectangle,Triangle and circle were in one alone class i did it
    for the sake of polymorphism because as overloading code with too many classes with non useful functions is baseless. 
    After- Separate classes are not added because now the program will be able to do make much more mathematical calculations on Square,Rectangle,Triangle and circle and now Square,Rectangle,Triangle and circle have their own classes and with their own function, Did this for the sake of simplicity so that the code looks readable and easy to understand.
    Will be working on code much more and new functions will be added ;)
    Have a good day if you are reading this.

--- AreaOfObjects.cpp
+++ AreaOfObjects.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Calculations
+class SquareCalculations
 {
 public:
     //int side;
 
-    double area_of_square(long side1)
+    long area_of_square(long side1)
     {
         std::cout << "The area of square will be: " << side1 * side1;
         return 0;
     }
-
+};
+class TriangleCalculations
+{   public:
     double area_of_triangle(double base_OT, double height_OT)
     {
 
@@ -20,13 +22,20 @@ public:
         std::cout << "The Area of the Triangle will be : " << answer_AOT;
         return 0;
     }
+};
 
+class RectangleCalculations
+{   public:
     double area_of_rectangle(double length_OR, double breadth_OR)
     {
         double answer_AOR = length_OR * breadth_OR;
         std::cout << "The area of Rectangle will be: " << length_OR * breadth_OR;
         return 0;
     }
+};
+
+class CircleCalculations
+{   public:
 
     double area_of_circle(double radius_OC)
     {
@@ -37,22 +46,22 @@ public:
 int main()
 {
 
-    std::cout << "<<<<<<<<<<<<<<<<<<<<      SIMPLE OBJECT'S AREA CALCULATOR BY *SharonIV*>>>>>>>>>>>>>>>>>>>>       "<<std::endl;
+    std::cout << "<<<<<<<<<<<<<<<<<<<<      SIMPLE OBJECT'S AREA CALCULATOR BY *SharonIV*>>>>>>>>>>>>>>>>>>>>       " << std::endl;
     std::cout << "You can perform calculations on following figures: " << std::endl;
 
-    std::cout << "Press 1 to perform calculation on Square" << std::endl;
-    std::cout << "Press 2 to perform calculation on Triangle" << std::endl;
-    std::cout << "Press 3 to perform calculation on Rectangle" << std::endl;
-    std::cout << "Press 4 to perform calculation on Circle" << std::endl;
+    std::cout << "\n\tPress 1 to perform calculation on Square" << std::endl;
+    std::cout << "\n\tPress 2 to perform calculation on Triangle" << std::endl;
+    std::cout << "\n\tPress 3 to perform calculation on Rectangle" << std::endl;
+    std::cout << "\n\tPress 4 to perform calculation on Circle" << std::endl;
 
     char figure_selection;
     std::cout << "Enter a CHOICE ~~~~~~~~~~~>  ";
     std::cin >> figure_selection;
 
-    if (figure_selection == '1')
+    if (figure_selection = '1')
     {
-        Calculations sq1;
-        long long square_side1;
+        SquareCalculations sq1;
+        long square_side1;
         // std::cout << "Enter the name of the figure ";
         // std::cin >> figure_selection;
 
@@ -61,11 +70,11 @@ int main()
         sq1.area_of_square(square_side1);
     }
 
-    else if (figure_selection == '2')
+    else if (figure_selection = '2')
     {
         double base_AOt, heightAOt;
 
-        Calculations AOT;
+        TriangleCalculations AOT;
         std::cout << "Enter the base of the triangle: ";
         std::cin >> base_AOt;
 
@@ -74,29 +83,28 @@ int main()
 
         AOT.area_of_triangle(base_AOt, heightAOt);
     }
-    else if (figure_selection == '3')
+    else if (figure_selection = '3')
     {
 
-        Calculations AOR;
+        RectangleCalculations AOR;
         double length_AOr, breadth_AOr;
         std::cout << "Enter LENGTH and BREADTH of the rectangle: ";
         std::cin >> length_AOr >> breadth_AOr;
         AOR.area_of_rectangle(length_AOr, breadth_AOr);
     }
-    else if (figure_selection == '4')
+    else if (figure_selection = '4')
     {
-        Calculations AOC;
+        CircleCalculations AOC;
         double radius_OC;
         std::cout << "Enter the radius of circle: ";
         std::cin >> radius_OC;
         AOC.area_of_circle(radius_OC);
     }
-    // if (figure_selection != '1' , figure_selection != '2',  figure_selection != '3' , figure_selection != '4');
-
-    // {
-    //     string error_code = "here is the error code 0x397bFFfeeE";
-    //     std::cout << "Please enter a valid CHOICE! " << error_code;
-    // }
+    while (figure_selection != '1' && figure_selection != '2' && figure_selection != '3' && figure_selection != '4')
+    {
+        std::cout << "Please enter a right option! ";
+        break;
+    }
 
     return 0;
 }
