88d0e55a498af87f507fca71116472ee09535206
SharonIV <91114837+SharonIV0x86@users.noreply.github.com>
2021-11-21 00:42:12 +0530

    Update FunsNClasses.cpp

--- FunsNClasses.cpp
+++ FunsNClasses.cpp
@@ -40,110 +40,77 @@ void clear_screen_and_delay_cross_platform()
 class SquareCalculations
 {
 public:
-    long area_of_square(long side1)
+    void area_of_square(long side1)
     {
-        double area_of_square;
+
         clear_screen_and_delay_cross_platform();
 
         std::cout << "\n\n\tThe area of square will be: " << side1 * side1 << " With side " << side1;
-
-        return 0;
-    }
-    double perimeter_of_square(double sidex4)
+        }
+    void perimeter_of_square(double sidex4)
     {
         double perimeter_of_sq = 4 * sidex4;
         clear_screen_and_delay_cross_platform();
         std::cout << "\n\n\tThe perimeter of the square will be: " << perimeter_of_sq << " with side " << sidex4 << std::endl;
-
-        return perimeter_of_sq;
     }
 };
 class TriangleCalculations
 {
 public:
-    double area_of_triangle(double base_OT, double height_OT)
+    void area_of_triangle(double base_OT, double height_OT)
     {
-
-        double answer_AOT = (0.5) * base_OT * height_OT;
-
         std::cout << "\n\n\tBase of triangle: " << base_OT;
         std::cout << "\n\n\tHeight of triangle: " << height_OT;
         clear_screen_and_delay_cross_platform();
-        std::cout << "\n\n\tThe Area Of Triangle will be 1/2 x " << base_OT << " x " << height_OT << " =  " << answer_AOT;
-
-        return 0;
+        std::cout << "\n\n\tThe Area Of Triangle will be 1/2 x " << base_OT << " x " << height_OT << " =  " << (0.5) * base_OT * height_OT;
     }
-    double perimeter_of_triangle(double side_a, double side_b, double side_c)
+    void perimeter_of_triangle(double side_a, double side_b, double side_c)
     {
         std::cout << "\n\n\tThe perimeter of the triangle is side1 + side2 + side3." << std::endl;
         clear_screen_and_delay_cross_platform();
         std::cout << "\n\n\tPerimeter of triangle with side 1, = " << side_a << " side 2, = " << side_b << " side 3, = " << side_b;
         std::cout << "\n\n\tThe perimeter of the triangle will be: " << side_a + side_b + side_c;
-
-        return 0;
     }
-    double area_of_triangle_herons(float side1, float side2, float side3)
+    void area_of_triangle_herons(float side1, float side2, float side3)
     {
-
-        float semi_peri = side1 + side2 + side3;
-        semi_peri /= 2;
-
-        float area = sqrt(semi_peri * (semi_peri - side1) * (semi_peri - side2) * (semi_peri - side3));
         clear_screen_and_delay_cross_platform();
-        std::cout << "\n\n\tSemi-Perimeter of the sides will be: " << semi_peri << std::endl;
-        std::cout << "\n\n\tArea of triangle will be: " << area;
-
-        return 0;
+        std::cout << "\n\n\tSemi-Perimeter of the sides will be: " << (side1 + side2 + side3) / 2;
+        std::cout << "\n\n\tArea of triangle will be: " << sqrt(((side1 + side2 + side3) / 2) * (((side1 + side2 + side3) / 2) - side1) * (((side1 + side2 + side3) / 2) - side2) * (((side1 + side2 + side3) / 2) - side3));
     }
 };
-
 class RectangleCalculations
 {
 public:
-    double area_of_rectangle(double length_OR, double breadth_OR)
+    void area_of_rectangle(double length_OR, double breadth_OR)
     {
-        double answer_AOR = length_OR * breadth_OR;
         clear_screen_and_delay_cross_platform();
         std::cout << "\n\n\tThe area of Rectangle will be: " << length_OR * breadth_OR;
-
-        return 0;
     }
-    float diagonal_of_rectangle(float length, float breadth)
+    void diagonal_of_rectangle(float length, float breadth)
     {
-        float diagonal = sqrt(pow(length, 2) + pow(breadth, 2));
         clear_screen_and_delay_cross_platform();
         std::cout << "\n\n\tThe DIAGONAL of rectangle with length: " << length << " and breadth " << breadth << std::endl;
-        std::cout << "\n\n\tWIll BE: " << diagonal;
-
-        return 0;
+        std::cout << "\n\n\tWIll BE: " << sqrt(pow(length, 2) + pow(breadth, 2));
     }
-    double perimeter_of_rectangle(double LENGTH, double BREADTH)
+    void perimeter_of_rectangle(double LENGTH, double BREADTH)
     {
-        double perimeter = 2 * (LENGTH + BREADTH);
         clear_screen_and_delay_cross_platform();
-        std::cout << "\n\n\tThe PERIMETER of rectangle will be: " << perimeter;
-
-        return 0;
+        std::cout << "\n\n\tThe PERIMETER of rectangle will be: " << 2 * (LENGTH + BREADTH);
     }
 };
 
 class CircleCalculations
 {
 public:
-    double area_of_circle(double radius_OC)
+    void area_of_circle(double radius_OC)
     {
         clear_screen_and_delay_cross_platform();
-
         std::cout << "\n\n\tThe area of CIRCLE will be: " << 3.14 * radius_OC * radius_OC;
-
-        return 0;
     }
-    double circumference_of_circle(double radius)
+    void circumference_of_circle(double radius)
     {
         clear_screen_and_delay_cross_platform();
         std::cout << "\n\n\tThe CIRCUMFERENCE of the CIRCLE will be: " << 2 * 3.14 * radius;
-
-        return 0;
     }
 };
 void delay_only_delay()
